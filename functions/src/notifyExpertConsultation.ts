7457f7c243c843b471f10344231e935c17b47108
LDrago27 <38322563+LDrago27@users.noreply.github.com>
2021-08-08 21:12:22 +0530

    Notification for New class

--- functions/src/notifyExpertConsultation.ts
+++ functions/src/notifyExpertConsultation.ts
@@ -18,7 +18,7 @@ router.get('/notifyExpertforNewConsultation?:expertId', async (req: any , res: a
         var docName = 'data';
         var clientDataAssociatedtoExpert = await db.collection(pathForNewClient).doc(docName).get();
 
-        console.log(clientDataAssociatedtoExpert);
+        //console.log(clientDataAssociatedtoExpert);
 
         if(!(clientDataAssociatedtoExpert)){
             return res.status(501).json("Unable to confirm client's association with ExpertId");
@@ -53,7 +53,7 @@ router.get('/notifyExpertforNewConsultation?:expertId', async (req: any , res: a
         var click_Action = "sample action";
         
         var response = await notificationService.sendNotification(latestToken,Notificationtitle,Notificationbody,click_Action);
-        console.log(response);
+        console.log(response.data);
         if(response.data.failure === 1){
             return res.status(111).json("Error Sending the Notifcation: Debug Info"+response.data.results);
         }
@@ -63,4 +63,65 @@ router.get('/notifyExpertforNewConsultation?:expertId', async (req: any , res: a
 
 
 });
+
+router.get('/notifyExpertforNewClass?:expertId', async (req: any , res: any ) => {
+
+    try{
+
+        var expertId = req.query.expertId;
+        console.log(expertId);
+        if(!(expertId)){
+            return res.status(400).json("Enter Valid expertId");
+        }
+
+        var pathForNewClient = '/experts/data/uid/'+ expertId+'/members/';
+        var docName = 'data';
+        var clientDataAssociatedtoExpert = await db.collection(pathForNewClient).doc(docName).get();
+
+        //console.log(clientDataAssociatedtoExpert);
+
+        if(!(clientDataAssociatedtoExpert)){
+            return res.status(501).json("Unable to confirm client's association with ExpertId");
+        }
+
+        var requestedClientsArray = clientDataAssociatedtoExpert.data().requestedClients;
+        var latestClient = requestedClientsArray[requestedClientsArray.length-1];
+
+        var clientName = latestClient.clientName;
+        var className = latestClient.className;
+
+        console.log(clientName,className);
+        
+        // Using clientName and packageName to populate the Notification Title
+
+        var pathForExpertToken = '/experts/data/uid/'+expertId+'/private/';
+        var docNameforExpertToken = 'token';
+
+        var tokenDocResponse = await db.collection(pathForExpertToken).doc(docNameforExpertToken).get();
+
+        if(!(tokenDocResponse)){
+            return res.status(501).json("No token found for the Expert");
+        }
+
+        var tokenList = tokenDocResponse.data().token;
+        var latestToken = tokenList[tokenList.length-1];
+
+        console.log(latestToken);   
+
+        var Notificationtitle = 'New Request to join the Class'+className;
+        var Notificationbody = clientName+ ' is requesting to join '+className +' Classroom';
+        var click_Action = "sample action";
+        
+        var response = await notificationService.sendNotification(latestToken,Notificationtitle,Notificationbody,click_Action);
+        console.log(response.data);
+        if(response.data.failure === 1){
+            return res.status(111).json("Error Sending the Notifcation: Debug Info"+response.data.results);
+        }
+
+        return res.status(200).json("Success");
+    }catch (error) { return res.status(501).json(error.message); }
+
+
+});
+
 export{ router as notifyExpert};
